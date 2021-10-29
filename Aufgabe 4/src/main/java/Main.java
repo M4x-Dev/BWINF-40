ea620f0df4e33351dee9e2fa25278c87f2ffbc6c
Maximilian Flügel <commandermax@fluegels.net>
2021-10-29 20:00:50 +0200

    Started working on task 4

---
+++ Aufgabe 4/src/main/java/Main.java
@@ -0,0 +1,25 @@
+/**
+ * Hauptklasse des Programmes
+ */
+public class Main {
+
+    public static String INPUT_FILE = "src/main/resources/wuerfel0.txt";
+    public static String OUTPUT_FILE = "output.txt";
+
+    /**
+     * Hauptmethode des Programmes.
+     *
+     * @param args Konsolenargumente des Programmes.
+     */
+    public static void main(String[] args) {
+        if(args.length > 0) INPUT_FILE = args[0]; //Eingabedatei kann auch als Konsolenparameter angegeben werden
+        if(args.length > 1) OUTPUT_FILE = args[1]; //Ausgabedatei kann auch als Konsolenparameter angegeben werden
+
+        //Laden der Eingabedatei
+        System.out.println();
+        System.out.println("Bundeswettbewerb für Informatik - Aufgabe 4 (Würfelglück) - by Maximilian Flügel");
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("Würfel \"" + INPUT_FILE + "\" werden geladen...");
+    }
+
+}
